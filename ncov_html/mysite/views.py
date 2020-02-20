ab24586cb74647a65f83f9e901ed20c9452ae165
Tong Jin <tongjin@Tongs-MacBook-Pro.local>
2020-02-19 20:08:03 -0600

    add another three provinces in the figure

--- ncov_html/mysite/views.py
+++ ncov_html/mysite/views.py
@@ -15,14 +15,46 @@ def homepage(request):
 	    province.append(list(p.values()))
 	x = [Time(p[-1]//1000,'posix').to('datetime')  for p in province]
 	y = [p[0] for p in province]
+    
+	province = []
+	for p in provinces.find({"provinceName":"河南省"},{'_id':0,'currentConfirmedCount': 1,
+	 'confirmedCount': 1, 'suspectedCount': 1, 'curedCount': 1, 'deadCount': 1,
+	 'updateTime':1}).sort('updateTime'):
+	    province.append(list(p.values()))
+	x1 = [Time(p[-1]//1000,'posix').to('datetime')  for p in province]
+	y1 = [p[0] for p in province]
+	province = []
+	for p in provinces.find({"provinceName":"江苏省"},{'_id':0,'currentConfirmedCount': 1,
+	 'confirmedCount': 1, 'suspectedCount': 1, 'curedCount': 1, 'deadCount': 1,
+	 'updateTime':1}).sort('updateTime'):
+	    province.append(list(p.values()))
+	x2 = [Time(p[-1]//1000,'posix').to('datetime')  for p in province]
+	y2 = [p[0] for p in province]
 
-	plot = figure(title = 'Shandong, CurrentConfirmed', x_axis_type = 'datetime',x_axis_label = 'Date',y_axis_label = 'Count',plot_width = 800,plot_height = 400)
+	province = []
+	for p in provinces.find({"provinceName":"浙江省"},{'_id':0,'currentConfirmedCount': 1,
+	 'confirmedCount': 1, 'suspectedCount': 1, 'curedCount': 1, 'deadCount': 1,
+	 'updateTime':1}).sort('updateTime'):
+	    province.append(list(p.values()))
+	x3 = [Time(p[-1]//1000,'posix').to('datetime')  for p in province]
+	y3 = [p[0] for p in province]
 
-	plot.line(x,y,line_width = 2)
-	plot.diamond(x,y,size = 10)
+	plot = figure(title = 'Shandong, Henan, Jiangsu, Current Infected people count', 
+		x_axis_type = 'datetime',x_axis_label = 'Date',y_axis_label = 'Count',
+		plot_width = 800,plot_height = 400)
+
+
+	plot.line(x,y,line_width = 2, legend_label = 'Shandong')
+	plot.line(x1,y1,line_width = 2,color = 'black',legend_label = 'Henan')
+	plot.line(x2,y2,line_width = 2,color = 'green',legend_label = 'Jinangsu')
+	plot.line(x3,y3,line_width = 2,color = 'orange',legend_label = 'zhejiang')
+
+	
 
 	script,div = components(plot)
 	#return render(request, 'pages/base.html', {})
 	return render(request,'pages/base.html',{'script':script,'div':div})
+
+	
 
 # Create your views here.
